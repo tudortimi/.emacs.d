85cf955c5322553b6e6c19ef90f7bef26b2db63a
Tudor Timi <tudortimi@users.noreply.github.com>
2017-12-18 22:58:00 +0100

    Disable startup screen

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (setq verilog-mode-file (expand-file-name "verilog_mode_settings.el" user-emacs-directory))
 (load verilog-mode-file)
 
+(setq inhibit-startup-screen t)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
